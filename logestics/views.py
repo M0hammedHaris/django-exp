2fd38bae1d3728d1cfb9b7a7b05c7d0929dbe45e
Mohammed Haris <mohammedharis.k.01@gmail.com>
2020-07-22 22:46:14 +0530

    Display required data

--- logestics/views.py
+++ logestics/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import *
 # Create your views here.
 
 
@@ -9,7 +9,23 @@ def home(request):
 
 
 def dashboard(request):
-    return render(request, 'logestics/dashboard.html')
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+
+    total_customers = customers.count
+    total_orders = orders.count
+    pending_orders = orders.filter(status = 'Pending').count()
+    delivered_orders = orders.filter(status = 'Delivered').count()
+
+    context = {
+        'customers': customers,
+        'orders': orders,
+        'total_customers': total_customers,
+        'total_orders': total_orders,
+        'pending_orders': pending_orders,
+        'delivered_orders': delivered_orders,
+    }
+    return render(request, 'logestics/dashboard.html', context=context)
 
 
 def customer(request):
